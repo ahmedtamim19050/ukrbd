8bfece10a112db913f2e8d9a66bf64048f4b0fd9
KawshikBss <kawshikbss@gmail.com>
2024-04-20 18:03:26 +0600

    modified send message link

--- resources/views/pages/store_front.blade.php
+++ resources/views/pages/store_front.blade.php
@@ -40,10 +40,7 @@
                             <div class="widget widget-collapsible widget-contact">
                                 <h3 class="widget-title"><span>Contact Vendor</span></h3>
                                 <div class="widget-body">
-                                    <input type="text" class="form-control" name="name" id="name" placeholder="Your Name" />
-                                    <input type="text" class="form-control" name="email" id="email_1" placeholder="you@example.com" />
-                                    <textarea name="message" maxlength="1000" cols="25" rows="6" placeholder="Type your messsage..." class="form-control" required="required"></textarea>
-                                    <a href="#" class="btn btn-dark btn-rounded">Send Message</a>
+                                    <a href="{{route('massage.create', ['id' => $shop->id])}}" class="btn btn-dark btn-rounded">Send Message</a>
                                 </div>
                             </div>
                             <!-- End of Widget -->
