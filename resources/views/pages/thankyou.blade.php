44ebd9c4bbae6ea8d1c9808f785e31bc61c2b823
Shuvo <102598399+Shuvoakonda@users.noreply.github.com>
2024-04-22 13:53:04 +0600

    thankyou

--- resources/views/pages/thankyou.blade.php
+++ resources/views/pages/thankyou.blade.php
@@ -1,17 +1,24 @@
-@extends('layouts.app')
-@section('css')
-    <link rel="stylesheet" href="{{ asset('assets/frontend-assets/css/style.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/frontend-assetss/responsive.css') }}" />
-    <link rel="stylesheet" id="bg-switcher-css" href="{{ asset('assets/frontend-assetss/css/backgrounds/bg-4.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/frontend-assets/css/shops.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/store_front.css') }}">
-@endsection
-@section('content')
-    <x-app.header />
+<x-app>
+    <x-slot name='css'>
+        <link rel="preload" href="assets/vendor/fontawesome-free/webfonts/fa-regular-400.woff2" as="font"
+            type="font/woff2" crossorigin="anonymous">
+        <link rel="preload" href="assets/vendor/fontawesome-free/webfonts/fa-solid-900.woff2" as="font"
+            type="font/woff2" crossorigin="anonymous">
+        <!-- Vendor CSS -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}">
+        <!-- Plugins CSS -->
+        <link rel="stylesheet" href="{{ asset('assets/vendor/swiper/swiper-bundle.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/animate/animate.min.css') }}">
+        <link rel="stylesheet" type="text/css"href="{{ asset('assets/vendor/magnific-popup/magnific-popup.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/demo5.min.css') }}">
+        <!-- Default CSS -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.min.css') }}">
+    </x-slot>
+
     <!-- Ec Thank You page -->
-    <section class="ec-thank-you-page section-space-p">
+    <div class="page-content d-flex align-items-center justify-content-center" style="height: 70vh;">
         <div class="container">
-            <div class="row">
+            <div class="row text-center mt-5">
                 <div class="col-md-12">
                     <div class="ec-thank-you section-space-p">
                         <!-- thank content Start -->
@@ -23,60 +30,32 @@
                             </div>
                         </div>
                         <!--thank content End -->
-                        <div class="ec-hunger">
+                        <div class="ec-hunger mb-5">
                             <div class="ec-hunger-detial">
                                 <h3>Want to track your order?</h3>
                                 <h6>Just click the link below.</h6>
-                                <a href="{{ route('user.ordersIndex') }}" class="btn btn-danger rounded">Track Order</a>
+                                <a href="{{ route('user.ordersIndex') }}" class="btn btn-primary btn-rounded">Track Order</a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
-    <section class="section ec-new-product section-space-p">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12 text-center">
-                    <div class="section-title">
-                        <h2 class="ec-bg-title">New Arrivals</h2>
-                        <h2 class="ec-title">New Arrivals</h2>
-                        <p class="sub-title">Browse The Collection of Top Products</p>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <!-- New Product Content -->
-                <div class="ec-spe-section  data-animation=" slideInLeft">
-
-                    <div class="ec-spe-products">
-                        @foreach ($latest_products->chunk(6) as $products)
-                            <div class="ec-fs-product">
-                                <div class="ec-fs-pro-inner">
-
-                                    <div class="row">
-                                        @foreach ($products as $product)
-                                            <x-products.product-3 :product="$product" />
-                                        @endforeach
+    </div>
 
 
+    <x-slot name='js'>
+        <script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+        <script src="{{ asset('assets/vendor/jquery/jquery.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/jquery.plugin/jquery.plugin.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/sticky/sticky.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/imagesloaded/imagesloaded.pkgd.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/jquery.countdown/jquery.countdown.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/magnific-popup/jquery.magnific-popup.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/jquery.gmap/jquery.gmap.min.js') }}"></script>
 
-                                    </div>
-
-                                </div>
-                            </div>
-                        @endforeach
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </section>
-@endsection
-@section('js')
-    <script src="{{ asset('assets/frontend-assets/js/vendor/jquery.magnific-popup.min.js') }}"></script>
-    <script src="{{ asset('assets/frontend-assets/js/plugins/jquery.sticky-sidebar.js') }}"></script>
-
-    <script src="{{ asset('assets/frontend-assets/js/main.js') }}"></script>
-@endsection
+        <!-- Main JS -->
+        <script src="{{ asset('assets/js/main.min.js') }}"></script>
+    </x-slot>
+</x-app>
