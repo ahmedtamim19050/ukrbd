5ddcbebc8e418a811c23e53b04c4435691d32649
Mahfuzul Islam <mahfuzul1125@gmail.com>
2024-06-28 19:07:53 +0600

    all data from import file is in collection and can be use by productDataImport weight works

--- app/Imports/ProductDataImport.php
+++ app/Imports/ProductDataImport.php
@@ -1,35 +1,39 @@
 <?php
-
 namespace App\Imports;
 
 use App\Services\ProductImportService;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Illuminate\Support\Str;
 
 class ProductDataImport implements ToCollection, WithHeadingRow
 {
-
     /**
      * @param Collection $collection
      */
     public function collection(Collection $collection)
     {
-    
         foreach ($collection as $data) {
-            $row = [
-                'sku'=>'',
-                'name' => $data['name'],
+          
+            $uniqueId = substr(Str::uuid()->toString(), 0, 8);
+            $slug = Str::slug($data['product_name']) . '-' . $uniqueId;
+            $title = $data['product_name'];
+            
+            $row = new Collection([
+                'sku' => '',
+                'name' => $data['product_name'],
                 'quantity' => $data['quantity'],
-                'description' =>$data['description'],
-                'slug' => $data['slug'],
-                'price' => $data['price'],
-                'sale_price' => $data['sale_price'],
+                'description' => $data['description'],
+                'slug' => $slug,
+                'price' => $data['purchase_price'],
+                'sale_price' => $data['sell_price'],
                 'status' => $data['status'],
                 'short_description' => $data['short_description'],
-                'weight' => $this->$data['weight'],
+                'weight' => $this->getWeightFromTitle($title),
                 'dimensions' => $data['dimension'],
-            ]
+            ]);
+            dd($row);
             (new ProductImportService($row))->import();
         }
     }
@@ -43,7 +47,6 @@ class ProductDataImport implements ToCollection, WithHeadingRow
             return $weight;
         } else {
             return 'N/A';
-
         }
     }
 }
