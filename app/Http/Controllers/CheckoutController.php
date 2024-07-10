5b11e0e4b791beeec279521a93c4c63b573f73b6
Ahmed Tamim <ahmedtamim19050@gmail.com>
2024-07-10 16:00:48 +0600

    issue

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -143,7 +143,7 @@ class CheckoutController extends Controller
                 Earning::create([
                     'order_id' => $childOrder->id,
                     'shop_id' => $childOrder->shop->id,
-                    'retailer_id' => $childOrder->shop->retailer_id,
+                    'retailer_id' => $childOrder->shop->referral_id,
                     'shop_earn' => Sohoj::shopWoned($childOrder),
                     'admin_earn' => Sohoj::adminOwned($childOrder),
                     'retailer_earn' => Sohoj::marchentigerOwned($childOrder),
