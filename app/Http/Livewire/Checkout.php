62c428f03b222b69d5c3759bc1431a061e154219
Kazi-Rayhan <thisiskazi@gmail.com>
2024-08-08 00:32:53 +0600

    done

--- app/Http/Livewire/Checkout.php
+++ app/Http/Livewire/Checkout.php
@@ -87,6 +87,14 @@ class Checkout extends Component
         $data = [];
         foreach (Cart::getContent() as $product) {
 
+            dd([
+                "store_id" => $product->attributes['store_id'],
+                "item_type" => 2,
+                "delivery_type" => 48,
+                "item_weight" => $product->attributes['weight'] > 0 ? $product->attributes['weight'] : 0.5,
+                "recipient_city" => explode('-', $this->selectedCity)[0],
+                "recipient_zone" => explode('-', $this->selectedZone)[0]
+            ]);
             // $response =   PathaoCourier::order()->priceCalculation([
             //     "store_id" => $product->attributes['store_id'],
             //     "item_type" => 2,
