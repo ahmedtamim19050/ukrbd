129c4b36c09f4c284d8a1893771efedcd4d395af
Kazi Rayhan Reza Tirtho <thisiskazi@gmail.com>
2024-09-19 19:50:01 +0600

    done

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -125,12 +125,11 @@
             object-fit: cover;
             aspect-ratio: 6/2 !important;
         }
-        Wolmart.$window.on("resize", function() {
-    var mobileMenu = t('.mobile-menu-wrapper');
-    if (mobileMenu.hasClass("keep-open")) {
-        mobileMenu.css("display", "block");
-    }
-});
+
+        body.menu-open {
+            overflow: hidden;
+            height: 100%;
+        }
     </style>
     {{ $css ?? null }}
     @stack('css')
